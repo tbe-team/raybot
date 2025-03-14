d4a3df9445c4194a144a9d5fdb34f426fbd198d1
tuanvumaihuynh <tuanvux01@gmail.com>
2025-03-14 11:28:16 +0700

    fix: date error when parse default value

--- internal/storage/db/migration/20250313015139_robot_state.sql
+++ internal/storage/db/migration/20250313015139_robot_state.sql
@@ -20,13 +20,13 @@ INSERT INTO robot_state (
 	drive_motor_state,
 	location_state
 ) VALUES (
-	'{"Current": 0, "Temp": 0, "Voltage": 0, "CellVoltages": [0, 0, 0, 0], "Percent": 0, "Fault": 0, "Health": 0, "UpdatedAt": "0000-00-00 00:00:00"}',
-	'{"CurrentLimit": 0, "Enabled": true, "UpdatedAt": "0000-00-00 00:00:00"}',
-	'{"CurrentLimit": 0, "Enabled": true, "UpdatedAt": "0000-00-00 00:00:00"}',
-	'{"FrontDistance": 0, "BackDistance": 0, "DownDistance": 0, "UpdatedAt": "0000-00-00 00:00:00"}',
-	'{"CurrentPosition": 0, "TargetPosition": 0, "IsRunning": false, "Enabled": true, "UpdatedAt": "0000-00-00 00:00:00"}',
-	'{"Direction": 0, "Speed": 0, "IsRunning": false, "Enabled": true, "UpdatedAt": "0000-00-00 00:00:00"}',
-	'{"CurrentLocation": "", "UpdatedAt": "0000-00-00 00:00:00"}'
+	'{"Current": 0, "Temp": 0, "Voltage": 0, "CellVoltages": [0, 0, 0, 0], "Percent": 0, "Fault": 0, "Health": 0, "UpdatedAt": "2023-01-01T00:00:00Z"}',
+	'{"CurrentLimit": 0, "Enabled": true, "UpdatedAt": "2023-01-01T00:00:00Z"}',
+	'{"CurrentLimit": 0, "Enabled": true, "UpdatedAt": "2023-01-01T00:00:00Z"}',
+	'{"FrontDistance": 0, "BackDistance": 0, "DownDistance": 0, "UpdatedAt": "2023-01-01T00:00:00Z"}',
+	'{"CurrentPosition": 0, "TargetPosition": 0, "IsRunning": false, "Enabled": true, "UpdatedAt": "2023-01-01T00:00:00Z"}',
+	'{"Direction": 0, "Speed": 0, "IsRunning": false, "Enabled": true, "UpdatedAt": "2023-01-01T00:00:00Z"}',
+	'{"CurrentLocation": "", "UpdatedAt": "2023-01-01T00:00:00Z"}'
 );
 -- +goose StatementEnd
 
