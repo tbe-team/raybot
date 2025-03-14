a8d51aa126a2a68ce1866636465a383973ca9b73
tuanvumaihuynh <tuanvux01@gmail.com>
2025-03-14 23:05:54 +0700

    chore(ui): run make build

---
+++ ui/dist/assets/CardTitle.vuevuetypescriptsetuptruelang-DCPJTw2G.js
@@ -0,0 +1,6 @@
+import{c as d,a as c}from"./createLucideIcon-D42RGvnV.js";import{d as r,D as n,e as l,G as t,b as o,a as i}from"./index-BwOykBdK.js";/**
+ * @license lucide-vue-next v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=d("CircleAlertIcon",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]]),_=r({__name:"CardHeader",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),n("div",{class:t(o(c)("flex flex-col gap-y-1.5 p-6",s.class))},[i(a.$slots,"default")],2))}}),y=r({__name:"CardTitle",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),n("h3",{class:t(o(c)("font-semibold leading-none tracking-tight",s.class))},[i(a.$slots,"default")],2))}});export{u as C,_,y as a};
