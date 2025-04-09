6d7d771e3f92f0245d1fd924693efcfe06dcf538
nguyen083 <billbatri085@gmail.com>
2025-04-09 14:22:30 +0700

    chore: rename useListAvailableSerialPorts

--- ui/src/composables/use-peripheral.ts
+++ ui/src/composables/use-peripheral.ts
@@ -2,7 +2,7 @@ import type { AxiosRequestConfig } from 'axios'
 import peripheralsAPI from '@/api/peripherals'
 import { useQuery } from '@tanstack/vue-query'
 
-export function useListAvailableSerialPorts(axiosOpts?: Partial<AxiosRequestConfig>) {
+export function useListAvailableSerialPortsQuery(axiosOpts?: Partial<AxiosRequestConfig>) {
   return useQuery({
     queryKey: ['serial-ports'],
     queryFn: () => peripheralsAPI.listAvailableSerialPorts(axiosOpts),
