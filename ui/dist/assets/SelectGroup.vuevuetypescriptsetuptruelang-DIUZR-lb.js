14eaf5b4b22879721564e6065b7145970d633d67
tuanvumaihuynh <tuanvux01@gmail.com>
2025-05-06 15:20:14 +0700

    chore: run pnpm build

---
+++ ui/dist/assets/SelectGroup.vuevuetypescriptsetuptruelang-DIUZR-lb.js
@@ -0,0 +1,6 @@
+import{P as d,c as m,a as _}from"./createLucideIcon-B1KZTuC_.js";import{g as f,c as y}from"./PopperContent-BATPYHgH.js";import{d as c,c as l,o as n,w as p,r as u,m as i,u as a,a as x}from"./index-DyKXIu_s.js";const[G,C]=y("SelectGroup"),g=c({__name:"SelectGroup",props:{asChild:{type:Boolean},as:{}},setup(o){const e=o,s=f(void 0,"reka-select-group");return C({id:s}),(t,r)=>(n(),l(a(d),i({role:"group"},e,{"aria-labelledby":a(s)}),{default:p(()=>[u(t.$slots,"default")]),_:3},16,["aria-labelledby"]))}});/**
+ * @license lucide-vue-next v0.503.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=m("circle-alert",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]]),v=c({__name:"SelectGroup",props:{asChild:{type:Boolean},as:{},class:{}},setup(o){const e=o,s=x(()=>{const{class:t,...r}=e;return r});return(t,r)=>(n(),l(a(g),i({class:a(_)("p-1 w-full",e.class)},s.value),{default:p(()=>[u(t.$slots,"default")]),_:3},16,["class"]))}});export{b as C,v as _};
