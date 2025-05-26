9b66525f45f470dbe7a80b782fd0f833107b9f29
nguyen083 <billbatri085@gmail.com>
2025-05-26 09:45:20 +0700

    feat: fixed header

--- ui/src/components/layouts/main-layout/MainLayout.vue
+++ ui/src/components/layouts/main-layout/MainLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useLocalStorage } from '@vueuse/core'
+import clsx from 'clsx'
 import { Notification, Notivue } from 'notivue'
 import ConfirmationDialog from '@/components/shared/ConfirmationDialog.vue'
 import ScrollToTopButton from '@/components/shared/ScrollToTopButton.vue'
@@ -15,20 +16,23 @@ const open = useLocalStorage('sidebar', true)
   <Notivue v-slot="item">
     <Notification :item="item" />
   </Notivue>
-
   <ConfirmationDialog />
-
   <SidebarProvider v-model:open="open">
     <AppSidebar />
     <SidebarInset>
-      <header class="flex items-center justify-between h-12 border-b shrink-0">
-        <div class="flex items-center gap-2 px-4">
-          <SidebarTrigger class="-ml-1" />
-          <Separator orientation="vertical" class="h-4 mr-2" />
+      <header
+        class="fixed top-0 right-0 z-10 transition-[left,right,width] !duration-200 ease-linear bg-white shadow"
+        :class="clsx(open ? 'lg:left-56 left-0' : 'lg:left-12 left-0')"
+      >
+        <div class="flex items-center justify-between h-12 border-b shrink-0">
+          <div class="flex items-center gap-2 px-4">
+            <SidebarTrigger class="-ml-1" />
+            <Separator orientation="vertical" class="h-4 mr-2" />
+          </div>
+          <HeaderActions class="mr-4" />
         </div>
-        <HeaderActions class="mr-4" />
       </header>
-      <main class="flex flex-col flex-1">
+      <main class="flex flex-col flex-1 mt-12">
         <RouterView />
       </main>
     </SidebarInset>
