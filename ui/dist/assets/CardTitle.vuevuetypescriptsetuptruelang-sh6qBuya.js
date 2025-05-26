9c5522fcd4238b7f148404a9a5e490e9cc5c4094
nguyen083 <billbatri085@gmail.com>
2025-05-26 10:18:54 +0700

    chore: run pnpm build

--- ui/dist/assets/CardTitle.vuevuetypescriptsetuptruelang-sh6qBuya.js
+++ ui/dist/assets/CardTitle.vuevuetypescriptsetuptruelang-sh6qBuya.js
@@ -1,4 +1,4 @@
-import{c as t,a as r}from"./http-B4KzYEr5.js";import{d as o,p as n,o as c,a as l,q as p,u as d}from"./index-DxwmmwYy.js";/**
+import{c as t,a as r}from"./http-Bd78JY7i.js";import{d as o,p as n,o as c,a as l,q as p,u as d}from"./index-BZJ3Cu-_.js";/**
  * @license lucide-vue-next v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
---
+++ ui/dist/assets/use-system-CcAS1d5j.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-D97yhmLI.js";import{i as t}from"./http-Bd78JY7i.js";const o={reboot(){return t.post("/system/reboot")},stopEmergency(){return t.post("/system/stop-emergency")}};function r(){return e({mutationFn:o.reboot})}function u(){return e({mutationFn:o.stopEmergency})}export{r as a,u};
