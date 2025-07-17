c5affa1c020b045f959cd6777249e2f735dada04
tuanvumaihuynh <tuanvux01@gmail.com>
2025-07-17 10:30:03 +0700

    chore: ignore esp disconnect error

--- internal/handlers/event/esp_event_handler.go
+++ internal/handlers/event/esp_event_handler.go
@@ -2,11 +2,8 @@ package event
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"time"
-
-	"golang.org/x/sync/errgroup"
 
 	"github.com/tbe-team/raybot/internal/events"
 	"github.com/tbe-team/raybot/internal/services/appstate"
@@ -25,33 +22,12 @@ func (s *Service) HandleESPSerialConnectedEvent(ctx context.Context, _ events.ES
 }
 
 func (s *Service) HandleESPSerialDisconnectedEvent(ctx context.Context, event events.ESPSerialDisconnectedEvent) {
-	g, ctx := errgroup.WithContext(ctx)
-	g.Go(func() error {
-		var errStr string
-		if event.Error != nil {
-			errStr = event.Error.Error()
-		}
-
-		if err := s.appStateService.UpdateESPSerialConnection(ctx, appstate.UpdateESPSerialConnectionParams{
-			Connected:    false,
-			SetConnected: true,
-			Error:        &errStr,
-			SetError:     true,
-		}); err != nil {
-			return fmt.Errorf("failed to update ESP serial connection: %w", err)
-		}
-
-		return nil
-	})
-
-	g.Go(func() error {
-		if err := s.systemService.SetStatusError(ctx); err != nil {
-			return fmt.Errorf("failed to set system status error: %w", err)
-		}
-		return nil
-	})
-
-	if err := g.Wait(); err != nil {
+	if err := s.appStateService.UpdateESPSerialConnection(ctx, appstate.UpdateESPSerialConnectionParams{
+		Connected:    false,
+		SetConnected: true,
+		Error:        ptr.New(event.Error.Error()),
+		SetError:     true,
+	}); err != nil {
 		s.log.Error("failed to update ESP serial connection", slog.Any("error", err))
 	}
 }
